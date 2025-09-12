0aa35d78692cc311074e3c51fd42dab2d9003ad6
nevijs <ducvien.cse@gmail.com>
2025-09-12 23:09:43 +0700

    Refactor Home component to use Next.js Link for navigation
    - Replaced anchor tags with Next.js Link components for improved client-side navigation.
    - Ensured consistency in linking to personal website and social media accounts.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,10 +11,10 @@ export default function Home() {
         <p>I am a part time indie hacker and a full-time software engineer, building products and sharing my journey here</p>
         <p>Find me on:</p>
         <li>
-          <a href="/">My website</a>
+          <Link href="/">My website</Link>
         </li>
         <li>
-          <a href="https://x.com/viennguyen_me">My X(Twitter) account</a>
+          <Link href="https://x.com/viennguyen_me">My X(Twitter) account</Link>
         </li>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
