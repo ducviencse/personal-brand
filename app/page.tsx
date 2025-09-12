8c3a82a9e45cc1ee593cdc6219c3a47d865ad3ed
nevijs <ducvien.cse@gmail.com>
2025-09-12 23:06:48 +0700

    Update layout and home page with personal branding and remove unused SVG files
    - Changed metadata in layout.tsx to reflect personal branding.
    - Updated Home component with a personal introduction and links to social media.
    - Removed unused SVG files from the public directory.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,17 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p>I am Tony, a software developer pursing the indie-hacker dream.</p>
-        <p>I can do UI design, do frontend, backend stuffs, a bit of Devops stuffs</p>
-        <p>I had experiences most on web development</p>
-        <p>This is a place where I share my thoughts, my knowledge, my experiences, also products I build </p>
+        <h1>Hello!</h1>
+        <p>My name is Tony Nguyen</p>
+        <Image src="/me.jpeg" width={512} height={512} alt="avatar"/>
+        <p>I am a part time indie hacker and a full-time software engineer, building products and sharing my journey here</p>
+        <p>Find me on:</p>
+        <li>
+          <a href="/">My website</a>
+        </li>
+        <li>
+          <a href="https://x.com/viennguyen_me">My X(Twitter) account</a>
+        </li>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
        
